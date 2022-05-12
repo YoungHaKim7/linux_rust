cae65ba373e1017354e7143c7aad8d92c2a96c03
YoungHa Kim <ytok1108@aol.com>
2022-05-12 19:38:27 +0900

    Droopin__dropped struct(usize)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use myvec::MyVec;
-fn main() {
-    let mut vec = MyVec::<usize>::new();
-    vec.push(1usize);
-    vec.push(2);
-    vec.push(3);
-    vec.push(4);
-    vec.push(5);
 
-    for n in 0..vec.len() {
-        assert_eq!(vec.get(n), Some(&(n + 1)));
+#[derive(PartialEq, Debug)]
+struct Dropped(usize);
+
+impl Drop for Dropped {
+    fn drop(&mut self) {
+        println!("Droppin");
     }
-    assert_eq!(vec.get(3), Some(&4));
-    assert_eq!(vec.capacity(), 8);
-    assert_eq!(vec.len(), 5);
+}
+fn main() {
+    let mut vec = MyVec::<Dropped>::new();
+    vec.push(Dropped(1));
+    vec.push(Dropped(2));
+    // vec.push(3);
+    // vec.push(4);
+    // vec.push(5);
+    assert_eq!(vec.get(1), Some(&Dropped(2)));
+
+    // for n in 0..vec.len() {
+    //     assert_eq!(vec.get(n), Some(&(n + 1)));
+    // }
+    assert_eq!(vec.capacity(), 4);
+    assert_eq!(vec.len(), 2);
+    println!("End of main");
 }
