39ed8ac738e124c4fd8790afec19ff33bc9e66cb
YoungHa Kim <ytok1108@aol.com>
2022-05-12 19:30:06 +0900

    Implement Vector__005__for __ vec.len()__assert_eq__vec.get__Some(&(n+1)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use myvec::MyVec;
 fn main() {
-    let mut vec = Vec::<usize>::new();
+    let mut vec = MyVec::<usize>::new();
     vec.push(1usize);
     vec.push(2);
     vec.push(3);
     vec.push(4);
     vec.push(5);
 
+    for n in 0..vec.len() {
+        assert_eq!(vec.get(n), Some(&(n + 1)));
+    }
     assert_eq!(vec.get(3), Some(&4));
     assert_eq!(vec.capacity(), 8);
     assert_eq!(vec.len(), 5);
